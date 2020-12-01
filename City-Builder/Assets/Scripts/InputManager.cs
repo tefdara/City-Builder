e624acb76696bac123f4fb0eea921a7b2eecca88
Tef Dara <dara0et@gmail.com>
2020-12-01 14:26:13 +0000

    Buiding Prototype

---
+++ City-Builder/Assets/Scripts/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+
+    public LayerMask MouseInputMask;
+
+
+    private void Update()
+    {
+        GetInput();
+    }
+    public void GetInput()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+
+            if(Physics.Raycast(ray.origin, ray.direction, out hitInfo, Mathf.Infinity, MouseInputMask))
+            {
+                Vector3 point = hitInfo.point - transform.position;
+                Debug.Log(point);
+            }
+
+        }
+    }
+}
